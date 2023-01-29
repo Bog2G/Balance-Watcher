a7f0c8f6ecd163aeb4176641f2574f470fb6bda4
Melfric <wegweg3eg@abv.bg>
2023-01-29 23:01:22 +0200

    Trying to add coing Gecko API

--- popup.js
+++ popup.js
@@ -1,20 +1,28 @@
 // when the add coin button is clicked show the input form and hide the button
 document.getElementById("add-coin-button").addEventListener("click", function() {
+    populate();
     document.getElementById("add-coin-form").style.display = "block";
     document.getElementById("add-coin-button").style.display = "none";
 });
 
-// populate the select field
-const coins = ["Coin1", "Coin2", "Coin3", "Coin4", "Coin5"]
-let select = document.getElementById("coin-select")
-let options = coins.map(coin => `<option value="${coin}">${coin}</option>`).join("\n")
-select.innerHTML = options
+
+async function populate() {
+    console.log('populating');
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/list');
+    const coins = await res.json();
+  
+    let select = document.getElementById("coin-select");
+    let options = coins.map(coin => `<option value="${coin.id}">${coin.name}></option>`).join("\n");
+    select.innerHTML = options;
+  }
+
 
 document.getElementById("add-coin-submit").addEventListener("click", function() {
     let coin = document.getElementById("coin-select").value;
     let balance = document.getElementById("coin-balance").value;
     let price = 20;
     let newRow = document.createElement("tr");
+    newRow.style.cssText = "text-align: center;"
     let coinCell = document.createElement("td");
     coinCell.innerHTML = coin;
     let balanceCell = document.createElement("td");
