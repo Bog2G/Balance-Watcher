0b88bf540e615ffcbef45e257618c9252c8cdbd7
Melfric <wegweg3eg@abv.bg>
2023-01-30 13:43:11 +0200

    Added local storage for table rows

--- popup.js
+++ popup.js
@@ -12,7 +12,7 @@ async function populate() {
     const coins = await res.json();
   
     let select = document.getElementById("coin-select");
-    let options = coins.map(coin => `<option value="${coin.id}">${coin.name}></option>`).join("\n");
+    let options = coins.map(coin => `<option value="${coin.id}">${coin.name}</option>`).join("\n");
     select.innerHTML = options;
   }
 
@@ -33,12 +33,49 @@ document.getElementById("add-coin-submit").addEventListener("click", function() 
     newRow.appendChild(balanceCell);
     newRow.appendChild(priceCell);
     document.getElementById("coin-list").getElementsByTagName("tbody")[0].appendChild(newRow);
+
+    let createdRows = JSON.parse(localStorage.getItem("createdRows")) || [];
+    createdRows.push({ coin, balance, price });
+    localStorage.setItem("createdRows", JSON.stringify(createdRows));
+
+
+
     // Clear the selected coin, entered balance and the price
     document.getElementById("coin-select").selectedIndex = 0;
     document.getElementById("coin-balance").value = "";
+
+
+
     // Hide the add-coin-form and show the add-coin-button
     document.getElementById("add-coin-form").style.display = "none";
     document.getElementById("add-coin-button").style.display = "block";
 
 });
 
+function populateTable() {
+    let storedRows = JSON.parse(localStorage.getItem("createdRows")) || [];
+    let tableBody = document.getElementById("coin-list").getElementsByTagName("tbody")[0];
+    console.log(storedRows);
+    if (tableBody) {
+        storedRows.forEach(row => {
+            let newRow = document.createElement("tr");
+            newRow.style.cssText = "text-align: center;"
+            let coinCell = document.createElement("td");
+            coinCell.innerHTML = row.coin;
+            let balanceCell = document.createElement("td");
+            balanceCell.innerHTML = row.balance;
+            let priceCell = document.createElement("td");
+            priceCell.innerHTML = row.price;
+            newRow.appendChild(coinCell);
+            newRow.appendChild(balanceCell);
+            newRow.appendChild(priceCell);
+            tableBody.appendChild(newRow);
+        });
+    }
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded');
+    populateTable();
+  });
